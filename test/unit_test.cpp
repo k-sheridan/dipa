761f19c83fa67b3a3f13001cf8425e80a5f72b90
Trexter <sheridak@purdue.edu>
2017-06-28 23:33:01 -0400

    testing image registration

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -17,6 +17,9 @@
 #include "opencv2/video.hpp"
 
 #include "opencv2/reg/mapprojec.hpp"
+#include "opencv2/reg/mappergradproj.hpp"
+#include "opencv2/reg/mapper.hpp"
+#include "opencv2/reg/mapperpyramid.hpp"
 
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/message_filter.h>
@@ -26,45 +29,76 @@
 #include <tf/tf.h>
 #include <tf/tfMessage.h>
 
+#include <image_transport/image_transport.h>
+#include "std_msgs/String.h"
+#include <sstream>
+#include <cv_bridge/cv_bridge.h>
+
 #include <dipa/GridRenderer.h>
 
+cv::Mat first;
+bool firstSet = false;
 
+static void showDifference(const cv::Mat& image1, const cv::Mat& image2, const char* title)
+{
+	cv::Mat img1, img2;
+	image1.convertTo(img1, CV_32FC3);
+	image2.convertTo(img2, CV_32FC3);
+	if(img1.channels() != 1)
+		cvtColor(img1, img1, CV_RGB2GRAY);
+	if(img2.channels() != 1)
+		cvtColor(img2, img2, CV_RGB2GRAY);
+
+	cv::Mat imgDiff;
+	img1.copyTo(imgDiff);
+	imgDiff -= img2;
+	imgDiff /= 2.f;
+	imgDiff += 128.f;
+
+	cv::Mat imgSh;
+	imgDiff.convertTo(imgSh, CV_8UC3);
+	imshow(title, imgSh);
+}
+
+void imgcb(const sensor_msgs::ImageConstPtr& img, const sensor_msgs::CameraInfoConstPtr& cam)
+{
+	cv::Mat temp = cv_bridge::toCvShare(img, img->encoding)->image.clone();
+
+	if(!firstSet)
+	{
+		first = temp;
+		firstSet = true;
+		ROS_DEBUG("set first frame");
+		return;
+	}
+
+	showDifference(first, temp, "before");
+	cv::waitKey(30);
+
+	cv::reg::MapperGradProj mapper = cv::reg::MapperGradProj();
+	cv::reg::MapperPyramid mappPyr(mapper);
+	cv::Ptr<cv::reg::Map> mapPtr;
+	mappPyr.calculate(first, temp, mapPtr);
+
+	//get the result
+	cv::reg::MapProjec* mapProj = dynamic_cast<cv::reg::MapProjec*>(mapPtr.get());
+	mapProj->normalize();
+
+}
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "dipa_test");
 
+	ros::NodeHandle nh;
 
-	tf::Transform w2c1;
-	w2c1.setRotation(tf::Quaternion(1/sqrt(3), 1/sqrt(3), 0, 1/sqrt(3)));
-	//w2c1.setRotation(tf::Quaternion(1/sqrt(2), 1/sqrt(2), 0, 0));
-	w2c1.setOrigin(tf::Vector3(0, 0, 1));
+	image_transport::ImageTransport it(nh);
 
-	cv::Mat_<float> K = (cv::Mat_<float>(3, 3) << 300, 0, 300, 0, 300, 300, 0, 0, 1);
+	image_transport::CameraSubscriber quadDetectCameraSub = it.subscribeCamera("m7/camera/image_rect", 2, imgcb);
 
-	GridRenderer gr;
 
-	gr.setColors(cv::Vec3i(255, 255, 255), cv::Vec3i(0, 255, 0), cv::Vec3i(255, 0, 0));
 
-	gr.grid_size = 10;
-	gr.grid_spacing = 0.32;
-	gr.inner_line_thickness = 0.04;
-	gr.outer_line_thickness = 0.08;
-
-	gr.setW2C(w2c1);
-	gr.setSize(cv::Size(600, 600));
-	gr.setIntrinsic(K);
-
-	/*
-	cv::Mat test = gr.renderGrid();
-
-	cv::imshow("test", test);
-	cv::waitKey(30);*/
-
-	while(ros::ok())
-	{
-
-	}
+	ros::spin();
 
 	return 0;
 }
